ec3b1fcf509b443d2a75d03c281edb999c537567
DreamFlower14 <sunghh1004@naver.com>
2024-03-18 00:13:50 +0900

    6장까지 완료, 책 한번 더 읽고 노션에 정리 +  주석으로 추가 정리 필요

--- src/test/java/JUnitCycleTest.java
+++ src/test/java/JUnitCycleTest.java
@@ -6,7 +6,7 @@ public class JUnitCycleTest {
         System.out.println("@BeforeAll");
     }
 
-    @BeforeEach
+    @BeforeEach // 테스트 케이스를 시작하기 전마다 실행
     public void beforeEach(){
         System.out.println("@BeforeEach");
     }
@@ -24,12 +24,12 @@ public class JUnitCycleTest {
         System.out.println("test3");
     }
 
-    @AfterAll
+    @AfterAll   // 전체 테스트를 마치고 종료하기 전에 1회 실행하므로 메서드는 static 으로 선언
     static void afterAll(){
         System.out.println("@AfterAll");
     }
 
-    @AfterEach
+    @AfterEach  // 테스트 케이스를 종료하기 전마다 실행
     public void afterEach(){
         System.out.println("@AfterEach");
     }
