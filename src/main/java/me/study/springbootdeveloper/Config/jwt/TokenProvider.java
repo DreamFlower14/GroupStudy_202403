bb0f3b4cd44538c4d4e7ee9e7cf3aebd3d831dbb
DreamFlower14 <DreamFlower14@gamil.com>
2024-04-01 21:49:42 +0900

    Json Web Token 주석 추가

--- src/main/java/me/study/springbootdeveloper/Config/jwt/TokenProvider.java
+++ src/main/java/me/study/springbootdeveloper/Config/jwt/TokenProvider.java
@@ -4,17 +4,14 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import me.study.springbootdeveloper.domain.User.User;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
@@ -22,6 +19,8 @@ import java.util.Set;
 @RequiredArgsConstructor
 @Service
 public class TokenProvider {
+    // JwtProperties.java 에서 클래스에 @component 를 사용해 빈으로 등록
+    // 클래스 내에서 @RequiredArgsConstructor 를 사용해 final 로 생성
     private final JwtProperties jwtProperties;
 
     public String generateToken(User user, Duration expiredAt){
@@ -37,13 +36,13 @@ public class TokenProvider {
         Date now = new Date();
 
         return Jwts.builder()
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setIssuer(jwtProperties.getIssuer())
-                .setIssuedAt(now)
-                .setExpiration(expiry)
-                .setSubject(user.getEmail())
-                .claim("id", user.getId())
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)   // 헤더 typ : JWT
+                .setIssuer(jwtProperties.getIssuer())           // 내용 iss : sunghh1004@naver.com ( properties.yml 에서 설정한 값 )
+                .setIssuedAt(now)                               // 내용 iat : 현재 시간
+                .setExpiration(expiry)                          // 내용 exp : expiry 멤버 변수값
+                .setSubject(user.getEmail())                    // 내용 sub : 유저의 이메일
+                .claim("id", user.getId())                   // 클레임 id : 유저의 ID
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())   // 서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
                 .compact();
     }
 
@@ -53,7 +52,7 @@ public class TokenProvider {
     public boolean validToken(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(jwtProperties.getSecretKey())
+                    .setSigningKey(jwtProperties.getSecretKey())    // 비밀값으로 복호화
                     .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
